3cb278e04308b0929c604caec88bf4038abbc301
Woooong <cyts1001@ajou.ac.kr>
2018-07-29 13:23:23 +0900

    willams_R

--- stock/indicators/result.py
+++ stock/indicators/result.py
@@ -10,32 +10,32 @@ if __name__ == "__main__":
 
 from stock.models import *
 
-class Indicator_rsi():
+class Indicator_wr():
     def __init__(self):
         self.data = {}
 
     def get_result_indicator(self, stock):
-        indicatiors = Indicator_Rsi.objects.filter(stock=stock).all()
+        indicatiors = Indicator_Wr.objects.filter(stock=stock).all()
         stock_count = 0
         last_price = 0
         current_price = 1000000
-        for rsi in indicatiors:
-            if rsi.type == "bid":
-                current_price -= rsi.price
+        for wr in indicatiors:
+            if wr.type == "bid":
+                current_price -= wr.price
                 stock_count += 1
-            elif rsi.type == "ask":
+            elif wr.type == "ask":
                 if stock_count > 0:
-                    current_price += rsi.price
+                    current_price += wr.price
                     stock_count -= 1
-            last_price = rsi.price
+            last_price = wr.price
 
         profit = (current_price+(stock_count*last_price) - 1000000)/1000000
-        res = Indicator_Result(stock=stock, indicator='RSI', profit=profit)
+        res = Indicator_Result(stock=stock, indicator='WR', profit=profit)
         res.save()
 
 
 if __name__ == '__main__':
-    test = Indicator_rsi()
+    test = Indicator_wr()
     stocks = Stock.objects.exclude(code='000000').all()
     for stock in stocks:
         test.get_result_indicator(stock=stock)
---
+++ stock/indicators/williams_r.py
@@ -0,0 +1,86 @@
+from django import setup
+import os
+import numpy as np
+import sys
+
+if __name__ == "__main__":
+    proj_path = "/Users/woong/Documents/mirae"
+    sys.path.append(proj_path)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mirae.settings")
+    setup()
+
+import pandas as pd
+import talib
+from stock.models import *
+
+class Indicator_wr():
+    def __init__(self):
+        self.data = {}
+
+    def get_result_indicator(self, result, price, date, stock):
+        wr = Indicator_Wr(stock=stock, date=date, type=result, price=price)
+        wr.save()
+        return {'result': result, 'price': price, 'date': date}
+
+
+    def collect_regular_price_in_pandas(self, stock):
+        # 이 함수는 일봉 데이터를 읽어온다
+        price_data = Stock_Data.objects.filter(stock=stock).order_by('-id').all()
+        # print(price_data)
+        pandas_raw_data = {
+            'high': [],
+            'low': [],
+            'close': [],
+            'open': [],
+            'date': [],
+            'date_d': [],
+        }
+        for row in price_data:
+            pandas_raw_data['high'].append(row.highest * 1.0)
+            pandas_raw_data['low'].append(row.lowest * 1.0)
+            pandas_raw_data['close'].append(row.close * 1.0)
+            pandas_raw_data['open'].append(row.start * 1.0)
+            pandas_raw_data['date'].append(row.date)
+            pandas_raw_data['date_d'].append(row.date)
+
+        df = pd.DataFrame(pandas_raw_data)
+        df.set_index('date', inplace=True)
+        df.sort_index(level='date', ascending=True, inplace=True)
+
+        return df
+
+    def ta_wr_indicator(self, stock):
+        data = self.collect_regular_price_in_pandas(stock=stock)
+        price_main = data
+        date_main = data.date_d
+        will_r = talib.WILLR(price_main['high'].values, price_main['low'].values, price_main['close'].values,
+                             timeperiod=14)
+        print("-------------------------")
+        print(len(will_r))
+        print(len(price_main))
+        print(len(date_main))
+        print("-------------------------")
+
+        try:
+            for i in range(1, len(will_r)):
+                if will_r[i-1] < -80 and will_r[i] > -80:
+                    self.get_result_indicator('bid', price_main['close'][i], date_main[i], stock)
+                elif will_r[i-1] > -20 and will_r[i] < -20:
+                    self.get_result_indicator('ask', price_main['close'][i], date_main[i], stock)
+        except Exception as e:
+            print(e)
+            print("-------------------------")
+            print(stock.name)
+            print(len(will_r))
+            print(len(price_main))
+            print(len(date_main))
+            print("-------------------------")
+            pass
+
+
+if __name__ == '__main__':
+    test = Indicator_wr()
+    stocks = Stock.objects.exclude(code='000000').all()
+    for stock in stocks:
+        test.ta_wr_indicator(stock=stock)
+
