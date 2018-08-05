8364e0c5d4e679d58a86ecb04e775f6375737f64
Woooong <cyts1001@ajou.ac.kr>
2018-08-05 15:50:58 +0900

    민감도 및 유저 관심 종목 api

---
+++ stock/indicators/sensitive.py
@@ -0,0 +1,34 @@
+from django import setup
+import os
+import sys
+import operator
+
+if __name__ == "__main__":
+    proj_path = "/Users/woong/Documents/mirae"
+    sys.path.append(proj_path)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mirae.settings")
+    setup()
+
+from stock.models import *
+
+class Senstive():
+    def __init__(self):
+        self.data = {}
+
+    def get_sensitvie(self, stock):
+        indicatiors = Indicator_Result.objects.filter(stock=stock).all()
+        sensitive = {}
+        for idc in indicatiors:
+            sensitive[idc.indicator] = idc.profit
+        sensitive = sorted(sensitive.items(), key=operator.itemgetter(1), reverse=True)
+        res = Indicator_Sensitive(stock=stock, sensitive=sensitive)
+        res.save()
+
+
+
+if __name__ == '__main__':
+    test = Senstive()
+    stocks = Stock.objects.exclude(code='000000').all()
+    for stock in stocks:
+        test.get_sensitvie(stock=stock)
+
--- stock/views.py
+++ stock/views.py
@@ -8,23 +8,45 @@ from stock.models import *
 def index(request):
     return HttpResponse("Hello, world. You're at the data index.")
 
-def get_stock_estimate(user=None):
+def get_stock_estimate(request, user_id=None):
+    user = User.objects.filter(id=user_id).all()[0]
     reuslt_json = []
-    stock_list = Stock.objects.all()
-    for stock in stock_list:
+    stock_list = User_stock.objects.filter(user=user).all()
+    for user_stock in stock_list:
         try:
-            wr = Indicator_Wr.objects.filter(stock=stock).all()[0]
-            macd = Indicator_Macd.objects.filter(stock=stock).all()[0]
-            rsi = Indicator_Rsi.objects.filter(stock=stock).all()[0]
+            stock = user_stock.stock
+            sensitive = Indicator_Sensitive.objects.filter(stock=stock).all()[0].sensitive
+            sensitive = sensitive.replace('[', '').replace(']', '').split(',')
+            first = sensitive[0].replace('(', '').replace('\'', '')
+            second = sensitive[2].replace('(', '').replace('\'', '').replace(' ', '')
+            
+            if first == "WR":
+                first_idc = Indicator_Wr.objects.filter(stock=stock).order_by('-id').all()[0]
+            elif first == "CROSS":
+                first_idc = Indicator_Cross.objects.filter(stock=stock).order_by('-id').all()[0]
+            elif first == "RSI":
+                first_idc = Indicator_Rsi.objects.filter(stock=stock).order_by('-id').all()[0]
+            elif first == "MACD":
+                first_idc = Indicator_Macd.objects.filter(stock=stock).order_by('-id').all()[0]
+            
+            if second == "WR":
+                second_idc = Indicator_Wr.objects.filter(stock=stock).order_by('-id').all()[0]
+            elif second == "CROSS":
+                second_idc = Indicator_Cross.objects.filter(stock=stock).order_by('-id').all()[0]
+            elif second == "RSI":
+                second_idc = Indicator_Rsi.objects.filter(stock=stock).order_by('-id').all()[0]
+            elif second == "MACD":
+                second_idc = Indicator_Macd.objects.filter(stock=stock).order_by('-id').all()[0]
+
 
             json = {
                 'stock': stock.name,
-                'wr': wr.type,
-                'macd': macd.type,
-                'rsi': rsi.type
+                first: first_idc.type,
+                second: second_idc.type
             }
             reuslt_json.append(json)
         except Exception as e:
+            print(e)
             pass
 
     return HttpResponse(reuslt_json)
