c33c9a8a1cd9fdb4659f2584e42bbdc52366c055
이우람 <uram999@naver.com>
2018-08-05 16:16:46 +0900

    오타 수정

--- stock/urls.py
+++ stock/urls.py
@@ -5,9 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get_stock_estimate/<int:user_id>', views.get_stock_estimate, name='get_stock_estimate'),
-<<<<<<< HEAD
     path('get_stock_list', views.get_stock_list, name='get_stock_list')
-=======
-    path('get_user_stock/<int:user_id>', views.get_user_stock, name='get_user_stock')
->>>>>>> 1cc61e14af32193370e172206900c856bbd17bd7
+
 ]
